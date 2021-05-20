4e12e798a39d2773ba62e440baa6d6705fa7ab08
James Newman <jamie@mindful-education.co.uk>
2021-05-20 13:28:30 +0100

    Add cancel button focus

--- dev/modules/handle-swal-dom.js
+++ dev/modules/handle-swal-dom.js
@@ -71,7 +71,13 @@ var openModal = function(callback) {
 
   window.previousActiveElement = document.activeElement;
   var $okButton = $modal.querySelector('button.confirm');
-  $okButton.focus();
+  var $cancelButton = $modal.querySelector('button.cancel');
+  
+  if ($cancelButton.offsetParent) {
+    $cancelButton.focus();
+  } else {
+    $okButton.focus();
+  }
 
   setTimeout(function () {
     addClass($modal, 'visible');
